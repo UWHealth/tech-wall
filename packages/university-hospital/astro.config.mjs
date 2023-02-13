179a98aedd3265d2b07edd9936c3f41cd24e6a44
Chris Lee <cleecanth@gmail.com>
2023-02-13 15:26:25 -0600

    Upgrade npm packages, cleanup
    Updated astro to v2.
    Deleted some unused files.

--- packages/university-hospital/astro.config.mjs
+++ packages/university-hospital/astro.config.mjs
@@ -1,24 +1,24 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import 'dotenv/config';
-
+import mdx from "@astrojs/mdx";
 const IS_GITHUB = process.env.GITHUB_ACTIONS;
 const IS_JENKINS = process.env.JENKINS;
 const GITHUB_SITE = 'https://uwhealth.github.io';
 const GITHUB_BASE = '/tech-wall';
 const JENKINS_SITE = process.env.JENKINS_SITE || 'https://dev.uconnect.wisc.edu';
-const JENKINS_BASE = process.env.JENKINS_BASE || '/apps/tech-wall/university-hospital/';
+const JENKINS_BASE = process.env.JENKINS_BASE || '/apps/tech-wall/university-hospital/'; // const IS_PRD = process.env.CI || process.env.JENKINS ? true : false;
 
-// const IS_PRD = process.env.CI || process.env.JENKINS ? true : false;
+const site = IS_JENKINS && JENKINS_SITE || IS_GITHUB && GITHUB_SITE || undefined;
+const base = IS_JENKINS && JENKINS_BASE || IS_GITHUB && GITHUB_BASE || undefined;
 
-const site = (IS_JENKINS && JENKINS_SITE) || (IS_GITHUB && GITHUB_SITE) || undefined;
-const base = (IS_JENKINS && JENKINS_BASE) || (IS_GITHUB && GITHUB_BASE) || undefined;
-
+// https://astro.build/config
 export default defineConfig({
-	integrations: [svelte()],
+  integrations: [svelte(), mdx()],
   output: 'static',
   build: {
     format: 'file' // forces the use of a single file instead of nested folders
+
   },
   site,
   base

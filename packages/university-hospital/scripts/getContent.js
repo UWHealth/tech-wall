1a894964f3a67971c596510af7b03d0141b306b9
Chris Lee <cleecanth@gmail.com>
2023-10-31 14:11:22 -0500

    Fix public paths

--- packages/university-hospital/scripts/getContent.js
+++ packages/university-hospital/scripts/getContent.js
@@ -3,14 +3,14 @@ import path from 'node:path';
 
 const __dirname = get__name(import.meta.url, true);
 
+// const contentfulData = getContentfulEntries(); // TODO: implement
+
 syncAsset(
   path.resolve(__dirname, '../public/', 'remote-assets/Tech-Wall-Lake.mp4'),
-  'https://bynder.uwhealth.org/m/1b36dd22807177c4/original/Tech-Wall-Lake.mp4'
+  'https://bynder.uwhealth.org/m/1b36dd22807177c4/original/Tech-Wall-Lake.mp4' //TODO: sync assets referenced in contentfulData
 );
-
-// const contentfulData = getContentfulEntries(); TODO
 
 writeContentfulData(
   path.resolve(__dirname, '../src/content/layout/base.json'),
-  JSON.stringify({ video: '/remote-assets/Tech-Wall-Lake.mp4' }, null, 0) // TODO: will be replaced with `contentfulData`
+  JSON.stringify({ video: '/public/remote-assets/Tech-Wall-Lake.mp4' }, null, 0) // TODO: will be replaced with `contentfulData`
 );

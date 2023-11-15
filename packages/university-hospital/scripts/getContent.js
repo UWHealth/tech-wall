bdc699ef2be2feba1a1d5e7da9ed06cb238b1f13
Chris Lee <cleecanth@gmail.com>
2023-11-15 13:14:36 -0600

    Path fixes

--- packages/university-hospital/scripts/getContent.js
+++ packages/university-hospital/scripts/getContent.js
@@ -2,15 +2,17 @@ import { syncAsset, writeContentfulData, getContentfulEntries, get__name } from 
 import path from 'node:path';
 
 const __dirname = get__name(import.meta.url, true);
+const root = path.resolve(__dirname, '../');
 
 // const contentfulData = getContentfulEntries(); // TODO: implement
+const videoBG = path.resolve(__dirname, '../public/', 'remote-assets/video-background.mp4');
 
 syncAsset(
-  path.resolve(__dirname, '../public/', 'remote-assets/Tech-Wall-Lake.mp4'),
+  videoBG,
   'https://bynder.uwhealth.org/m/1b36dd22807177c4/original/Tech-Wall-Lake.mp4' //TODO: sync assets referenced in contentfulData
 );
 
 writeContentfulData(
   path.resolve(__dirname, '../src/content/layout/base.json'),
-  JSON.stringify({ video: '/public/remote-assets/video-background.mp4' }, null, 0) // TODO: will be replaced with `contentfulData`
+  JSON.stringify({ video: '/' + path.relative(root, videoBG) }, null, 0) // TODO: will be replaced with `contentfulData`
 );
